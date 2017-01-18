7d37462bf3bc38574b8b530739dce247f885ae9c
jhoun <jayhoun@gmail.com>
2017-01-18 09:42:09 -1000

    get works on handlebars

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,19 +2,42 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const Author = db.Author;
+const Post = db.Post;
 
 
 
 router.route('/')
-  .get((req,res) => {
-    console.log('adfllkajdf');
+  .get((req, res) => {
+    Post.findAll()
+    .then((post)  => {
+      console.log('post: ', post);
+      res.render('./index', {post});
+    })
+    .catch((e) =>{
+      console.error(e);
+      res.json(e);
+    });
   })
   .post((req,res) => {
-    console.log('hit');
-    Author.create({name: req.body.author})
-      .then((author) =>{
-        res.json(author);
+    Post.create({
+      author: req.body.author,
+      title: req.body.title,
+      link: req.body.link,
+      description: req.body.description
+    })
+      .then((post) =>{
+        res.json(post);
+      })
+      .catch((e) =>{
+        console.error(e);
+        res.json(e);
       });
   });
+
+router.route('/new')
+  .get((res, req) => {
+    res.render('./portfolio/new');
+  });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('./models');
 const portfolio = require('./routes/portfolio');
 
+
+
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'main',
+}))
+
+app.set('view engine', '.hbs');
 
 app.use(bodyParser.urlencoded({
   extended:true
@@ -13,9 +22,7 @@ app.use((req, res, next) => {
   next('route');
 })
 
-
 app.use('/portfolio', portfolio);
-
 
 app.listen(3000, function() {
   console.log('Server started on port 3000');
