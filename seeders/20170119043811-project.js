723e60d5f1eb98f20cd7511051b9d470d9dd8a4d
jhoun <jayhoun@gmail.com>
2017-01-23 18:17:44 -1000

    creating branch to work on seed

--- seeders/20170119043811-project.js
+++ seeders/20170119043811-project.js
@@ -1,5 +1,4 @@
 'use strict';
-
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Project', [{
@@ -9,7 +8,6 @@ module.exports = {
      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
     }], {});
   },
-
   down: function (queryInterface, Sequelize) {
     author: 'Lorem Ipsum'
   }
