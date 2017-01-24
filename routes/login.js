28861718dbab6b1d26fb6db3683dc2b16a8536d7
jhoun <jayhoun@gmail.com>
2017-01-23 16:50:44 -1000

    implemented bcrypt

--- routes/login.js
+++ routes/login.js
@@ -6,57 +6,81 @@ const LocalStrategy = require('passport-local').Strategy;
 const CONFIG = require('../config/config');
 const bcrypt = require('bcrypt');
 
-const saltRounds = 10;
-const myPlainTextPassword = "ILikeBacon";
+// const myPlainTextPassword = "ILikeBacon";
 
 
-bcrypt.genSalt(saltRounds, (err,salt) => {
-  if (err) {
-    console.error(err);
-  }
-  //runs only when new user is registered
-  bcrypt.hash(myPlainTextPassword, salt, (err, hash) => {
-    // save this hash to your DB
-    console.log('hash: ', hash);
+// bcrypt.genSalt(saltRounds, (err,salt) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   //runs only when new user is registered
+//   bcrypt.hash(myPlainTextPassword, salt, (err, hash) => {
+//     // save this hash to your DB
+//     // console.log('hash: ', hash);
 
-  bcrypt.compare(myPlainTextPassword, hash, (err, result) => {
-      if (err){
-        console.error(err);
-      }
-      //if it's the same - returns true
-      console.log('result1: ', result);
-    });
-  });
-});
+//   bcrypt.compare(myPlainTextPassword, hash, (err, result) => {
+//       if (err){
+//         console.error(err);
+//       }
+//       //if it's the same - returns true
+//       // console.log('result1: ', result);
+//     });
+//   });
+// });
 
-const authenticate = (username, password) => {
+// const authenticate = (username, password) => {
 
-  // get user data from DB ** THE ONLY THING YOU NEED TO CHANGE WHEN YOU ARE IN EXPRESS-GALLERY
-  return db.User.findOne({
+//   // get user data from DB ** THE ONLY THING YOU NEED TO CHANGE WHEN YOU ARE IN EXPRESS-GALLERY
+//   return db.User.findOne({
+//     where:{username}
+//   })
+//     .then((user) => {
+//       if (user === null) {
+//         throw Error("invalid user")
+//       }
+//       // if (user.password !== password){
+//       //   throw Error("invalid password")
+//       // }
+//       console.log('user.password: ', user.password);
+//       console.log('password: ', password);
+//       bcrypt.compare(password, user.password, (err, result) =>{
+//         if (result === true){
+//           console.log('result: ', result);
+//           console.log('user: ', user);
+//           return user;
+//         }
+//         if (result === false){
+//           throw Error("invalid password");
+//         }
+//       })
+//     })
+// }
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    return db.User.findOne({
     where:{username}
   })
     .then((user) => {
       if (user === null) {
         throw Error("invalid user")
       }
-      if (user.password !== password){
-        throw Error("invalid password")
-      }
-      return user;
-    })
-
-}
-
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    authenticate(username, password)
-      .then((user) => {
-        return done(null, user);
+      // if (user.password !== password){
+      //   throw Error("invalid password")
+      // }
+      console.log('user.password: ', user.password);
+      console.log('password: ', password);
+      bcrypt.compare(password, user.password, (err, result) =>{
+        if (result === true){
+          console.log('result: ', result);
+          console.log('user: ', user);
+          return done(null,user);
+        }
+        if (result === false){
+          return done(null, false);
+        }
       })
-      .catch((e) => {
-        console.log('e: ', e);
-        return done(e);
-      });
+    })
 
   }
 ));
