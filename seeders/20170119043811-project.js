a6406a387309d19b998ee3a3a9a7c1be5cdb8e39
jhoun <jayhoun@gmail.com>
2017-01-23 18:44:46 -1000

    created new seed for users

--- seeders/20170119043811-project.js
+++ seeders/20170119043811-project.js
@@ -1,11 +1,13 @@
 'use strict';
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Project', [{
+    return queryInterface.bulkInsert('Projects', [{
      author: 'Lorem Ipsum',
      title: "Neque porro quisquam est qui dolorem ipsum quia",
      link: "http://www.google.com",
-     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+     description: "Lorem ipsum dolor sit amet",
+     createdAt: "2017-01-23 18:31:04.857-10",
+     updatedAt: "2017-01-23 18:31:04.857-10"
     }], {});
   },
   down: function (queryInterface, Sequelize) {
---
+++ seeders/20170124043709-users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      first_name: 'John',
+      last_name: 'doe',
+      username: 'johnnydoe',
+      password: '1w809urjsoejr[woejir',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    first_name: 'John'
+  }
+};
