791d7a49e0c66471ca82c9007c7f65c6bcf5e5df
jhoun <jayhoun@gmail.com>
2017-01-26 00:04:32 -1000

    test

--- routes/login.js
+++ routes/login.js
@@ -5,86 +5,42 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const CONFIG = require('../config/config');
 const bcrypt = require('bcrypt');
+Promise.promisifyAll(bcrypt);
 
-// const myPlainTextPassword = "ILikeBacon";
+const authenticate = (username, password) => {
 
-
-// bcrypt.genSalt(saltRounds, (err,salt) => {
-//   if (err) {
-//     console.error(err);
-//   }
-//   //runs only when new user is registered
-//   bcrypt.hash(myPlainTextPassword, salt, (err, hash) => {
-//     // save this hash to your DB
-//     // console.log('hash: ', hash);
-
-//   bcrypt.compare(myPlainTextPassword, hash, (err, result) => {
-//       if (err){
-//         console.error(err);
-//       }
-//       //if it's the same - returns true
-//       // console.log('result1: ', result);
-//     });
-//   });
-// });
-
-// const authenticate = (username, password) => {
-
-//   // get user data from DB ** THE ONLY THING YOU NEED TO CHANGE WHEN YOU ARE IN EXPRESS-GALLERY
-//   return db.User.findOne({
-//     where:{username}
-//   })
-//     .then((user) => {
-//       if (user === null) {
-//         throw Error("invalid user")
-//       }
-//       // if (user.password !== password){
-//       //   throw Error("invalid password")
-//       // }
-//       console.log('user.password: ', user.password);
-//       console.log('password: ', password);
-//       bcrypt.compare(password, user.password, (err, result) =>{
-//         if (result === true){
-//           console.log('result: ', result);
-//           console.log('user: ', user);
-//           return user;
-//         }
-//         if (result === false){
-//           throw Error("invalid password");
-//         }
-//       })
-//     })
-// }
-
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    return db.User.findOne({
+  // get user data from DB ** THE ONLY THING YOU NEED TO CHANGE WHEN YOU ARE IN EXPRESS-GALLERY
+  return db.User.findOne({
     where:{username}
   })
     .then((user) => {
       if (user === null) {
         throw Error("invalid user")
       }
-      // if (user.password !== password){
-      //   throw Error("invalid password")
-      // }
-      console.log('user.password: ', user.password);
-      console.log('password: ', password);
-      bcrypt.compare(password, user.password, (err, result) =>{
-        if (result === true){
-          console.log('result: ', result);
-          console.log('user: ', user);
-          return done(null,user);
-        }
-        if (result === false){
-          return done(null, false);
+      return bcrypt.compareAsync(password, user.password)
+      .then((isValid) =>{
+        if(isValid){
+          return user;
+        } else {
+          return false;
         }
       })
     })
+}
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    authenticate(username, password)
+      .then((user) => {
+        return done(null, user);
+      })
+      .catch((e) => {
+        console.log('e: ', e);
+        return done(e);
+      });
 
   }
 ));
-
 passport.serializeUser((user, done) =>{
   //user is passed inf rom Local Strategy - only runs when a user first authenticate
   //user's session has is hashed
