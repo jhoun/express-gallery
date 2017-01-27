83a1cf51289d61efd9c3724592382738901dec89
jhoun <jayhoun@gmail.com>
2017-01-26 14:19:58 -1000

    updated config.json and package.json

--- seeders/20170124043709-users.js
+++ seeders/20170124043709-users.js
@@ -1,12 +1,10 @@
 'use strict';
+const CONFIG = require('../config/config');
 const bcrypt = require('bcrypt');
 
-const salt = bcrypt.genSaltSync(10)
-const newPassword = bcrypt.hashSync("secret", salt)
 
-console.log('newPassword: ', newPassword);
-
-
+const salt = bcrypt.genSaltSync(CONFIG.saltRounds)
+const newPassword = bcrypt.hashSync(CONFIG.seedPassword, salt)
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
